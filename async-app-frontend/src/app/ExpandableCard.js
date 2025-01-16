7d061fb9793a298af7007b0ce414ebfdbe4c2139
Maansi Bisht <Maansibisht466@gmail.com>
2025-01-16 22:49:37 +0530

    Update ExpandableCard.js

--- async-app-frontend/src/app/ExpandableCard.js
+++ async-app-frontend/src/app/ExpandableCard.js
@@ -5,16 +5,14 @@ const ExpandableCard = ({ chunkId, content, index, matchPercentage, path , html 
 
   return (
     <div className="border border-gray-300 rounded-lg shadow-md p-4 bg-white text-black">
-      {/* Title and Match Percentage */}
       <div className="flex justify-end items-center">
         <span className="text-green-600 text-sm font-medium">{`${matchPercentage}% match`}</span>
       </div>
-
-      {/* Toggle HTML Content */}  
       <pre className="mt-3 p-3 bg-gray-100 rounded text-xs overflow-x-auto text-left" style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
           {content}
       </pre>
       <p className="text-gray-500 text-sm mt-1 text-left">{`Path: ${path}`}</p>
+      {/* Toggle HTML Content */}  
       <button
         onClick={() => setIsExpanded(!isExpanded)}
         className="mt-3 text-blue-600 underline text-sm flex justify-start"
